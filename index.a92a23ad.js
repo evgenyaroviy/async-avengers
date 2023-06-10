0a0f3ce586ed841508a90276cdcc340de88034c1
evgenyaroviy <120398316+evgenyaroviy@users.noreply.github.com>
2023-06-10 15:03:12 +0000

    Deploying to gh-pages from @ evgenyaroviy/async-avengers@4a5ca806024609c29f64fe7e96ae9c5af0d37ecf 🚀

---
+++ index.a92a23ad.js
@@ -0,0 +1,2 @@
+const o=document.querySelector(".scroll-up-btn");window.addEventListener("scroll",(function(){window.pageYOffset>300?o.style.display="block":o.style.display="none"})),o.addEventListener("click",(function(){window.scrollTo({top:0,behavior:"smooth"})}));
+//# sourceMappingURL=index.a92a23ad.js.map
--- library.64dca206.js
+++
@@ -1,2 +0,0 @@
-!function(){var c="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkOTZiN2ExNTYwNGYwMmExYWNkMTVhNWJlY2JmMjQ4MCIsInN1YiI6IjY0ODNhYTBhOTkyNTljMDBlMmY0NWE4ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._Sdbi-2PalUFAI7K7hzIv-hc4p92EU6q_yg6_IJJHjA";"Bearer ".concat(c),"Bearer ".concat(c),"Bearer ".concat(c),"Bearer ".concat(c),"Bearer ".concat(c),"Bearer ".concat(c)}();
-//# sourceMappingURL=library.64dca206.js.map
