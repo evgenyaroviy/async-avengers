3b2e72f7f08dd400dab771f7a4432a697aa2c3d3
Viktoriia Protasova <viktorka.protasova@gmail.com>
2023-06-10 18:33:01 +0300

    Update gallery.js

--- src/js/pages/catalog/gallery.js
+++ src/js/pages/catalog/gallery.js
@@ -1,15 +1,57 @@
 import axios from 'axios';
-import { optionsWeek, optionsUpcoming, optionsSearch, optionsDetails, optionsVideos, optionsGenre } from '../../request';
-import { galleryMarkup } from "../../galleryMarkup";
+import {
+  optionsWeek,
+  optionsUpcoming,
+  optionsSearch,
+  optionsDetails,
+  optionsVideos,
+  optionsGenre,
+} from '../../request';
 
-// Приклад використання:
-console.log('Hello')
+import { galleryMarkup } from '../../galleryMarkup';
+import { ratingToStars } from '../../components/ratingAPI';
+
+const galleryContainer = document.querySelector('.gallery-container');
+
+const options = {
+  method: 'GET',
+  url: 'https://api.themoviedb.org/3/genre/movie/list',
+  params: { language: 'en' },
+  headers: {
+    accept: 'application/json',
+    Authorization:
+      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YTA1ZGZhMzkwMDIxYjkyZTc3ZDMzYzRhODYyZjRmNiIsInN1YiI6IjY0ODFmZWJmNjQ3NjU0MDBhZDgxYTBlMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.grPFkj7_KdeogJCliXg4MDIgfDdRvjZd4DM3BdVB2Kw',
+  },
+};
 
 axios
-    .request(optionsWeek)
-    .then(function (response) {
-        console.log(response.data);
-    })
-    .catch(function (error) {
-        console.error(error);
+  .request(options)
+  .then(function (response) {
+    // console.log(response.data);
+
+    const { genres: genresNew } = response.data;
+    console.log(genresNew);
+  })
+  .catch(function (error) {
+    console.error(error);
+  });
+
+axios
+  .request(optionsWeek)
+  .then(function (response) {
+    const { results: resultsNew } = response.data;
+
+    const resultsYears = resultsNew.map(r => {
+      r.release_date = r.release_date.slice(0, 4);
+      r.vote_average = ratingToStars(r.vote_average);
+      //   r.genre_ids = ratingToStars(r.vote_average);
+      return r;
     });
+
+    // console.log(resultsYears);
+
+    galleryContainer.innerHTML = galleryMarkup(response.data.results);
+  })
+  .catch(function (error) {
+    console.error(error);
+  });
