70fb5a9e084ffdf6e0205c7fa44f4b33905bfe66
tasita <uatasita@gmail.com>
2023-06-11 20:40:50 +0300

    galleryMarkup

--- src/catalog.js
+++ src/catalog.js
@@ -2,6 +2,7 @@ import './js/pages/header';
 import './js/pages/hero';
 import './js/pages/catalog/search';
 import './js/pages/catalog/gallery';
+import './js/pages/catalog/pagination';
 import './js/galleryMarkup';
 import './js/request';
 import './js/components/modal';
--- src/js/galleryMarkup.js
+++ src/js/galleryMarkup.js
@@ -1,30 +1,26 @@
+import { ratingToStars } from '../js/components/ratingAPI';
+
 export function galleryMarkup(movies) {
   return movies
-    .map(({ title, poster_path, genre_ids, vote_average, release_date }) => {
-      return `<div class="movie-card">
-        <img src="${poster_path}" alt="${title}" loading="lazy" class="movie-card__img"/>
-      <div class="movie-card__info">
-        <div>
-        <p class="info-item">
-          <b class="info-item__name">${title}</b>
-        </p>
+    .map(
+      ({ title, poster_path, genre_name, vote_average, release_date, id }) => {
+        const stars = ratingToStars(vote_average);
+        const releaseYear = release_date.slice(0, 4);
 
-        <p class="info-item">
-          <b class="info-item__name">${genre_ids}</b>
-        </p>
-        
-        <p class="info-item">
-          <b class="info-item__name">${release_date}</b>
-        </p>
-        </div>
-
-        <div>
-        <p class="info-item">
-          <b class="info-item__name">${vote_average}</b>
-        </p>
-        </div>
-      </div>
-    </div>`;
-    })
+        return `<li data-id=${id} class="movie-card"> 
+      <img src="https://image.tmdb.org/t/p/original${poster_path}" alt="${title}" loading="lazy" class="movie-card__img" width="280" height="406"/> 
+      <div class="movie-card__backdrop"> 
+        <div class="movie-card__info"> 
+          <p class="info-item__name">${title}</p> 
+           
+          <div class="movie-card__info__items"> 
+            <p class="info-item__genreYear">${genre_name} | ${releaseYear}</p> 
+            <div class="info-item__stars">${stars}</div> 
+          </div> 
+        </div> 
+      </div> 
+    </li>`;
+      }
+    )
     .join('');
 }
