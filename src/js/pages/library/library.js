e4538c142fbf92af6e625ed7a0b2b17a29e769ee
Mykokola <j0kfrbr@gmail.com>
2023-06-13 15:03:56 -0500

    add ganresfix add filter

--- src/js/pages/library/library.js
+++ src/js/pages/library/library.js
@@ -1,14 +1,36 @@
 import {galleryMarkup} from '../../galleryMarkup'
+
+const localMoviesList = JSON.parse(localStorage.getItem('MOVIE-ID-LIST'))
+const genresSelect = document.querySelector('.genres-select')
+
+genresSelect 
+if(genresSelect){
+genresSelect.addEventListener('change',libraryRender) }
 export function libraryRender(){
     try{
-    const localMoviesList = JSON.parse(localStorage.getItem('MOVIE-ID-LIST'))
     const moviesContainerEl = document.querySelector('.movies-libary')
     const oopsMessage = document.querySelector('.oops-message')
+    let localMoviesListGanresSort = []
     moviesContainerEl.innerHTML = ``
+    localMoviesList.forEach(e => {
+        e.genre_name = e.genres[0].name
+    })
+
+    localMoviesList.sort(e => {
+        e.genre_name === genresSelect.value
+    })
+
+    localMoviesList.forEach(e => {
+        if(e.genre_name === genresSelect.value){
+            localMoviesListGanresSort.unshift(e)
+        }else{
+            localMoviesListGanresSort.push(e)
+        }
+    })
     if(localMoviesList.length===0){
         oopsMessage.style.display = 'block'
     }else{
-        moviesContainerEl.insertAdjacentHTML('beforeend',galleryMarkup(localMoviesList))
+        moviesContainerEl.insertAdjacentHTML('beforeend',galleryMarkup(localMoviesListGanresSort))
     }
 }catch{
     //hi hi hi HARD CODE
