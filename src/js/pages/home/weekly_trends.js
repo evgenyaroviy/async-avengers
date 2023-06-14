4d2f3f8d13c4021d8d9bffc32e3317de175aabf0
M1seryy <62029606+M1seryy@users.noreply.github.com>
2023-06-14 16:56:47 +0300

    not working

--- src/js/pages/home/weekly_trends.js
+++ src/js/pages/home/weekly_trends.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { galleryMarkup } from '../../galleryMarkup';
-import { optionsWeek } from '../../request';
+import { optionsGenre, optionsWeek } from '../../request';
 
 const API_KEY = 'e80fd9fb75f14049ed52c4547080278b';
 const CARD_BLOCK = document.querySelector('.movies-container');
@@ -10,8 +10,10 @@ const getResponce = async () => {
     return res.then(res => {
       if (window.screen.width <= 768) {
         createMarkup(res.data.results.slice(0, 1));
+        getGenres(res.data.results.slice(0, 1));
       } else {
         createMarkup(res.data.results.slice(0, 3));
+        // generateGenres(res.data.results.slice(0, 1), getGenres);
       }
     });
   } catch (error) {
@@ -22,3 +24,27 @@ getResponce();
 function createMarkup(data) {
   CARD_BLOCK.innerHTML = galleryMarkup(data);
 }
+async function getGenres() {
+  try {
+    const res = await axios.request(optionsGenre);
+    return res.then(res => {
+      return res.data;
+    });
+  } catch (error) {}
+}
+
+console.log(getGenres());
+// function generateGenres(movieInfo, genres) {
+//   const genresName = [];
+//   movieInfo.forEach(movie => {
+//     const genreNames = [];
+//     movie.genre_ids.forEach(genreId => {
+//       const genre = genres.find(genre => genre.id === genreId);
+//       if (genre) {
+//         genreNames.push(genre.name);
+//       }
+//     });
+//     genresName.push(genreNames.join(', '));
+//   });
+//   return genresName;
+// }
